d3ba253515c89ad341ea4e336845c021d61a22b7
rafael36832 <rafael.brum@ufv.br>
2022-01-04 17:53:24 -0300

    Initial

---
+++ src/KotlinBasico/colecoes.kt
@@ -0,0 +1,23 @@
+package KotlinBasico
+
+fun colecoes(){
+
+    // Coleções do Java
+    // List, Array, Set e Map
+
+    // List
+    var lista = mutableListOf<Int>()
+    var listaImutável = listOf<Int>()
+
+    // Array (mutável)
+    var vetor = arrayListOf<Int>()
+
+    //Set
+    var conjunto = mutableSetOf<Int>()
+    var conjuntoImutavel = setOf<Int>()
+
+    //Map
+    var hash = mutableMapOf<Int, Int>()
+    var dictImutavel = mapOf<Int, Int>(Pair(2, 4), Pair(3, 9))
+}
+
