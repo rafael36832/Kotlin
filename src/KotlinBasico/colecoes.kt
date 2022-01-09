085dbe2120edbcd0c3d56696b415616fe801fdbb
rafael36832 <rafael.brum@ufv.br>
2022-01-08 22:35:11 -0300

    08/01/2022

--- src/KotlinBasico/colecoes.kt
+++ src/KotlinBasico/colecoes.kt
@@ -8,13 +8,16 @@ fun colecoes(){
     // List
     var lista = mutableListOf<Int>()
     var listaImutável = listOf<Int>()
+    // Funções add, remove, removeAt, size, contains, acesso por índice...
 
     // Array (mutável)
     var vetor = arrayListOf<Int>()
+    // Funções add, remove, removeAt, size, contains, acesso por índice...
 
     //Set
     var conjunto = mutableSetOf<Int>()
     var conjuntoImutavel = setOf<Int>()
+    // Funções add, remove, size, contains
 
     //Map
     var hash = mutableMapOf<Int, Int>()
